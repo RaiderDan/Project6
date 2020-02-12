93e800007dc754dcb5c6e0ca9cdaf8a43fb55e71
RaiderDan <60192075+RaiderDan@users.noreply.github.com>
2020-02-11 20:55:32 -0500

    Add files via upload

---
+++ Project_6_Collins.rb
@@ -0,0 +1,15 @@
+###############################################
+# Dan Collins
+# Project 6
+# Project_6_Collins.rb
+# #############################################
+# Part 1
+# 1. ask user their age and determine ticket price based on input
+# 2. output age and ticket price
+# 3. Ask user if they want to know the ticket price for another buyer
+#     -only accept y for yes, n for no. if y, prompt for another age. if n, exit the loop
+# 4. Output for each iteration
+# Part 2
+# 5. Use a for loop to calculate and display the accumulated revenue assuming 5 of each type of ticket is sold per show
+# #############################################
+puts "test"
